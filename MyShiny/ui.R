eed2841d8549eb808598b02d0bffc831344e37a6
Haijing Wang <sea.breeze.929@gmail.com>
2021-01-01 14:16:57 -0500

    add documentation to explain purpose and how to use the shiny app

--- MyShiny/ui.R
+++ MyShiny/ui.R
@@ -16,6 +16,15 @@ shinyUI(fluidPage(
     titlePanel("Shiny project with mtcars dataset"),
     
     p(textOutput("mydate")),
+    
+    h4("This application will plot mpg~weight for data in mtcars dataset. 
+      The scatter plot is colored by number of cylinders. 
+      For cars in a cylinder group, a regression line with confidence interval is also plotted."),
+      
+      
+    h4("User can select range of vehicle weight (in tons), by changing slider, 
+      both scatter plot and regression line will re-render 
+      based on filtered data."),
 
     sliderInput("slider1", label = h3("Weight(tons) Range"), min = 1, 
                         max = 6, step=0.25, width='60%', value = c(1, 6)),
